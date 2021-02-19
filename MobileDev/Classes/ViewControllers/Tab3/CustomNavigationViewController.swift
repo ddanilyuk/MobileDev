a76100ed251352946a2abb73497db981ad62729a
ddanilyuk <danis.danilyuk@gmail.com>
2021-02-19 10:44:07 +0200

    Code refactoring

--- MobileDev/Classes/ViewControllers/Tab3/CustomNavigationViewController.swift
+++ MobileDev/Classes/ViewControllers/Tab3/CustomNavigationViewController.swift
@@ -9,15 +9,9 @@ import UIKit
 
 final class CustomNavigationViewController: UINavigationController {
     
-    //    var backgroundImage: UIImage?
-    //    var shadowImage: UIImage?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        view.backgroundColor = .white
-        //        navigationBar.clipsToBounds = false
-        //        hide
         navigationBar.isTranslucent = true
     }
     
@@ -26,33 +20,24 @@ final class CustomNavigationViewController: UINavigationController {
         let viewController = super.popViewController(animated: animated)
         
         if let coordinator = transitionCoordinator, animated {
-            
-            print("add")
-            
+                        
             navigationBar.setBackgroundImage(nil, for: .default)
             navigationBar.shadowImage = nil
             navigationBar.tintColor = .accent
-            //            navigationBar.updateConstraints()
-            //            navigationBar.layoutIfNeeded()
             
             coordinator.animate(alongsideTransition: nil) { [weak self] context in
                 
-                
                 if context.isCancelled {
                     
-                    print("isCancelled")
                     self?.navigationBar.setBackgroundImage(UIImage(), for: .default)
                     self?.navigationBar.shadowImage = UIImage()
                     self?.navigationBar.tintColor = .white
-                    
-                    self?.navigationBar.layoutIfNeeded()
                 }
             }
         } else {
             
             navigationBar.setBackgroundImage(nil, for: .default)
             navigationBar.shadowImage = nil
-            navigationBar.layoutIfNeeded()
             navigationBar.tintColor = .accent
         }
         
@@ -68,23 +53,9 @@ final class CustomNavigationViewController: UINavigationController {
             backButton.title = ""
             
             navigationBar.topItem?.backBarButtonItem = backButton
-            
             navigationBar.tintColor = .white
-            
             navigationBar.setBackgroundImage(UIImage(), for: .default)
             navigationBar.shadowImage = UIImage()
-            navigationBar.layoutIfNeeded()
-        }
-        
-    }
-}
-
-
-extension UIColor {
-    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(x: 0, y: 0, width: size.width, height: size.height))
         }
     }
 }
--- MobileDev/Classes/ViewControllers/Tab3/MovieDetailViewController.swift
+++ MobileDev/Classes/ViewControllers/Tab3/MovieDetailViewController.swift
@@ -12,9 +12,8 @@ final class MovieDetailViewController: UIViewController {
     
     static func create(with movie: Movie) -> MovieDetailViewController {
         
-        let controller = UIStoryboard.main.instantiateViewController(withIdentifier: MovieDetailViewController.identifier)
-        
-        return controller as! MovieDetailViewController
+        let controller = UIStoryboard.main.instantiateViewController(withIdentifier: MovieDetailViewController.identifier) as! MovieDetailViewController
+        return controller
     }
     
     @IBOutlet weak var posterImageView: UIImageView!
@@ -24,6 +23,7 @@ final class MovieDetailViewController: UIViewController {
             tableDirector = TableDirector(tableView: tableView, scrollDelegate: self)
         }
     }
+    
     @IBOutlet weak var imageViewTrailing: NSLayoutConstraint!
     @IBOutlet weak var imageViewLeading: NSLayoutConstraint!
     @IBOutlet weak var imageViewTop: NSLayoutConstraint!
@@ -34,9 +34,10 @@ final class MovieDetailViewController: UIViewController {
     @IBOutlet weak var labelBottom: NSLayoutConstraint!
     
     @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var viewHeight: NSLayoutConstraint!
+    @IBOutlet weak var headerViewHeight: NSLayoutConstraint!
     
-    //    @IBOutlet weak var visualEffectText: UIVisualEffectView!
+    // MARK: - Private properties
+    
     private var tableDirector: TableDirector!
     
     // MARK: - Lifecycle
@@ -44,17 +45,25 @@ final class MovieDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+        setupGestures()
+        setupTableView()
+    }
+    
+    // MARK: - Setup methods
+    
+    private func setupView() {
+        
         movieTitleLabel.clipsToBounds = true
         
-        tableView.contentInset = UIEdgeInsets(top: 550,
-                                              left: 0,
-                                              bottom: -view.safeAreaInsets.bottom,
-                                              right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 550, left: 0, bottom: 0, right: 0)
         tableView.scrollIndicatorInsets = tableView.contentInset
         
-        viewHeight.constant = 550
+        headerViewHeight.constant = 550
         headerView.layoutIfNeeded()
-        
+    }
+    
+    private func setupGestures() {
         let upSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(didPan(_:)))
         upSwipeGestureRecognizer.direction = .up
         
@@ -63,20 +72,6 @@ final class MovieDetailViewController: UIViewController {
         
         headerView.addGestureRecognizer(upSwipeGestureRecognizer)
         headerView.addGestureRecognizer(downSwipeGestureRecognizer)
-        
-        setupTableView()
-    }
-    
-    @objc private func didPan(_ sender: UISwipeGestureRecognizer) {
-        
-        switch sender.direction {
-        case .down:
-            tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
-        case .up:
-            tableView.scrollRectToVisible(CGRect(x: 0, y: tableView.frame.height - 276, width: 1, height: 1), animated: true)
-        default:
-            break
-        }
     }
     
     private func setupTableView() {
@@ -97,12 +92,27 @@ final class MovieDetailViewController: UIViewController {
         }
         tableDirector.reload()
     }
+    
+    // MARK: - Actions
+    
+    @objc private func didPan(_ sender: UISwipeGestureRecognizer) {
+        
+        switch sender.direction {
+        case .down:
+            tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
+        case .up:
+            tableView.scrollRectToVisible(CGRect(x: 0, y: tableView.frame.height - 276, width: 1, height: 1), animated: true)
+        default:
+            break
+        }
+    }
 }
+
+// MARK: UIScrollViewDelegate
 
 extension MovieDetailViewController: UIScrollViewDelegate {
     
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        
+    func endDecelerating(_ scrollView: UIScrollView) {
         let yOffset = -scrollView.contentOffset.y
         
         guard yOffset > 275 else {
@@ -111,54 +121,39 @@ extension MovieDetailViewController: UIScrollViewDelegate {
         
         let newOffser = yOffset < 550 - (275 / 2) ? tableView.frame.height - 276 : 0
         tableView.scrollRectToVisible(CGRect(x: 0, y: newOffser, width: 1, height: 1), animated: true)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        endDecelerating(scrollView)
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         
-        let yOffset = -scrollView.contentOffset.y
-        
-        guard yOffset > 275 else {
-            return
-        }
-        
-        let newOffser = yOffset < 550 - (275 / 2) ? tableView.frame.height - 276 : 0
-        tableView.scrollRectToVisible(CGRect(x: 0, y: newOffser, width: 1, height: 1), animated: true)
+        endDecelerating(scrollView)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        var yOffset = -scrollView.contentOffset.y
-        print("yOffset", yOffset)
+        let yOffset = max(-scrollView.contentOffset.y, 275)
+        debugPrint("yOffset: ", yOffset)
         
-        let window = UIApplication.shared.windows[0]
-        let topPadding = window.safeAreaInsets.top
-        
-        if yOffset < 275 {
-            yOffset = 275
-            navigationController?.navigationBar.tintColor = .accent
-        }
-        
-        navigationController?.navigationBar.tintColor = .white
-        
+        let statusBarHeight = UIApplication.shared.windows[0].safeAreaInsets.top
+        let screenWidth = UIScreen.main.bounds.width
         let persent = abs(min(((yOffset - 275) / 275), 1) - 1)
         
         labelLeading.constant = max(150 * persent, 0)
         labelTrailing.constant = 8 * persent
-        labelBottom.constant = ((275 - topPadding - 100 - 8) / 2) * persent
-        
-        let screenWidth = UIScreen.main.bounds.width
-        
+        labelBottom.constant = ((275 - statusBarHeight - 100 - 8) / 2) * persent
+        movieTitleLabel.layer.cornerRadius = 10 * persent
         
         imageViewBottom.constant = (100 - (persent * 100)) + (8 * persent)
         imageViewTrailing.constant = (screenWidth - 142.0) * persent
         imageViewLeading.constant = 8 * persent
-        
-        imageViewTop.constant = topPadding * persent + 8 * persent
-        
+        imageViewTop.constant = statusBarHeight * persent + 8 * persent
         posterImageView.layer.cornerRadius = 10 * persent
-        movieTitleLabel.layer.cornerRadius = 10 * persent
         
-        viewHeight.constant = yOffset
+        headerViewHeight.constant = yOffset
         headerView.layoutIfNeeded()
     }
 }
